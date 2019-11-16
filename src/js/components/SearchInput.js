bc8b3c78a83e24784d0dd1d1820d4467929de4d1
Paul Kaspriskie <paulkaspriskie@MacBook-Pro.local>
2019-11-16 18:53:09 -0500

    added logic to capture input vale on enter

--- src/js/components/SearchInput.js
+++ src/js/components/SearchInput.js
@@ -3,10 +3,37 @@ import React from 'react';
 
 class SearchInput extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { inputValue: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.getInputValue = this.getInputValue.bind(this);
+  }
+
+  componentDidMount() {
+    const url = 'https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Patient?name=paul';
+
+  }
+
+  handleChange(e) {
+    this.setState({ inputValue: e.target.value });
+  }
+
+  getInputValue(e) {
+    if(e.keyCode == 13){
+      console.log(this.state.inputValue);;
+    }
+  }
+
   render() {
     return (
       <div className="input-wrapper">
-        <input type="text" defaultValue='Search'  />
+        <input
+          type="text"
+          defaultValue='Search'
+          onChange={this.handleChange}
+          onKeyDown={this.getInputValue}  />
       </div>
     );
   }
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import SearchInput from './components/SearchInput';
-const url = 'https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/';
+
 
 render(
   <div className="app-container">
