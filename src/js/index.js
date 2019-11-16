a5fb3ef6b02b26b9b33bdd0e3a261c527d8b4a06
Paul Kaspriskie <paulkaspriskie@MacBook-Pro.local>
2019-11-16 16:49:21 -0500

    added react reat-dom and @babel/preset-react dependencies

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
-console.log('hi');
+
+import React from 'react';
+import { render } from 'react-dom';
+
+
+render(
+  <div>
+    <h1>SMART-FHIR-APP</h1>
+  </div>,
+  document.getElementById('root')
+);
