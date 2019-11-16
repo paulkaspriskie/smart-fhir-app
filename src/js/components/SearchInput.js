d1fe532d7a4a5cadd3460a5581094a53c3b7b095
Paul Kaspriskie <paulkaspriskie@MacBook-Pro.local>
2019-11-16 17:35:27 -0500

    created SearchInput component

---
+++ src/js/components/SearchInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class SearchInput extends React.Component {
+
+  render() {
+    return (
+      <div className="input-wrapper">
+        <input type="text" defaultValue='Search'  />
+      </div>
+    );
+  }
+}
+
+export default SearchInput;
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-
+import SearchInput from './components/SearchInput';
+const url = 'https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/';
 
 render(
-  <div>
-    <h1>SMART-FHIR-APP</h1>
+  <div className="app-container">
+    <SearchInput />
   </div>,
   document.getElementById('root')
 );
